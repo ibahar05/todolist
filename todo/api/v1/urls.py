848b6af05020d75c375f89803f8065d30b05cc7a
Bahar <ibahare05@gmail.com>
2026-06-25 22:25:56 +0330

    fbv to cbv(APIView)

--- todo/api/v1/urls.py
+++ todo/api/v1/urls.py
@@ -5,7 +5,10 @@ from . import views
 app_name="api-v1"
 
 urlpatterns = [
-    path("tasks/",views.tasklist,name="todo"),
-    path("tasks/<id>",views.taskdetail, name="task-detail"),
+    # path("tasks/",views.tasklist,name="todo"),
+    # path("tasks/<id>",views.taskdetail, name="task-detail"),
+    path("tasks/",views.TaskList.as_view(),name="todo"),
+    path("tasks/<int:id>",views.TaskDetail.as_view(), name="task-detail"),
+
 
 ]
--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -5,11 +5,13 @@ from ...models import Task
 from .seializer import TaskSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 
-@api_view(["GET","POST"])
+'''@api_view(["GET","POST"])
 @permission_classes([IsAuthenticated])
 def tasklist(request):
+    """ V1 of task list view"""
     if request.method == "GET":
         tasks = Task.objects.all()
         seializer = TaskSerializer(tasks, many=True)
@@ -20,10 +22,31 @@ def tasklist(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    '''
+#---------------------------------------------------------------------------
+
+class TaskList(APIView):
+    """V2 of task list view"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        seializer = self.serializer_class(tasks, many=True)
+        return Response(seializer.data)
     
-@api_view(["GET","PUT", "DELETE"])
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+#---------------------------------------------------------------------------
+    
+'''@api_view(["GET","PUT", "DELETE"])
 @permission_classes([IsAuthenticated])
 def taskdetail(request, id):
+    """ V1 of task detail view"""
     task = get_object_or_404(Task, pk=id, user=request.user)
     if request.method == "GET":
         serializer = TaskSerializer(task)
@@ -37,7 +60,34 @@ def taskdetail(request, id):
     
     elif request.method == "DELETE":
         task.delete()
+        return Response({"detail":"task deleted successfully"}, status=status.HTTP_204_NO_CONTENT)'''
+
+#------------------------------------------------------------------------------------------------------
+
+class TaskDetail(APIView):
+    """ V2 of task detail view"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+
+    def get(self, request, id):
+        task = get_object_or_404(Task, pk=id, user=request.user)
+        serializer = self.serializer_class(task)
+        return Response(serializer.data)
+    
+    def put(self, request,id):
+        task = get_object_or_404(Task, pk=id, user=request.user)
+        serializer = self.serializer_class(task)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request,id):
+        task = get_object_or_404(Task, pk=id, user=request.user)
+        task.delete()
         return Response({"detail":"task deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
-        
+
+
+
+
         
