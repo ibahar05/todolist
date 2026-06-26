61b0889eaedee298e6b1f7c86a7bd1719df3cb03
Bahar <ibahare05@gmail.com>
2026-06-26 19:11:56 +0330

    adding absolute url to our api

--- todo/api/v1/seializer.py
+++ todo/api/v1/seializer.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from ...models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = Task
         fields = "__all__"
+
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
